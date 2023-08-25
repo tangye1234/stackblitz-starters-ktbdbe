503bbbdb773498d156ba4c2e719125f1ab332073
tangye1234 <447084+tangye1234@users.noreply.github.com>
2023-08-25 10:58:27 +0800

    2 files modified, 3 added, 3 deleted

--- components/masonry.tsx
+++ components/masonry.tsx
@@ -1,7 +1,4 @@
-'use client'
-
 import {
-  startTransition,
   useEffect,
   useImperativeHandle,
   useMemo,
---
+++ pages/index.tsx
@@ -0,0 +1,5 @@
+import { Masonry } from '@components/masonry';
+
+export default function Home() {
+  return <Masonry>Home</Masonry>;
+}
