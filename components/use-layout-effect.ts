c4103dd5970d61ca6808bbef11f06d6c3c633c33
tangye1234 <447084+tangye1234@users.noreply.github.com>
2023-08-25 13:44:31 +0800

    1 file modified

--- components/use-layout-effect.ts
+++ components/use-layout-effect.ts
@@ -1,8 +1,4 @@
 import { useLayoutEffect as useBaseLayoutEffect, useEffect } from 'react';
 
-export const useLayoutEffect: typeof useBaseLayoutEffect = (effect, deps?) => {
-  if (typeof window !== 'undefined') {
-    return useBaseLayoutEffect(effect, deps);
-  }
-  return useEffect(effect, deps);
-};
+export const useLayoutEffect =
+  typeof window === 'undefined' ? useEffect : useBaseLayoutEffect;
